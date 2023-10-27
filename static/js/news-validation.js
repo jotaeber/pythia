f2c61c63aa7da3bd121e5c366c5bd42bd17a968d
Jonatan Eberle <jonatan.eberle@outlook.com>
2023-10-27 16:29:20 -0300

    Nuevos cambios; falta ajustar news-validation, hacer faq-validation, y med queries de Programacion

--- static/js/news-validation.js
+++ static/js/news-validation.js
@@ -1,47 +1,40 @@
 
-const form = document.getElementById('formRegister');
-const formControls = document.getElementsByClassName('form-control');
-const selectCountry = document.getElementsByName('country');
-const inputs = document.getElementsByTagName('input');
-const select2 = document.querySelector('[name="country"]');
-const labels = document.querySelectorAll('.form-control label');
-
-//Creacion de nodos
-const formRegister = document.querySelector('#formRegister');
+// Nodos
+const formNewsletter = document.querySelector('#form-newsletter');
 const p = document.createElement('p');
-// const b = document.createElement('b');
-// b.textContent='No te olvides de completar el formulario';
-// p.appendChild(b);
-console.log(p);
-p.innerHTML = '<b>No te olvides de completar el formulario</b>';
-
-formRegister.appendChild(p);
 
 const validationForm = (event) => {
-    // alert('Estoy validando el formulario');
     event.preventDefault();
     
-    const firtsname = document.querySelector('#firstname');
-    const lastname = document.querySelector('#lastname');
+    const name = document.querySelector('#nombre');
+    const email = document.querySelector('#email');
+    const age = document.querySelector('#edad');
 
     let validation = true;
     
-    if(firtsname.value === ''){
-        firtsname.classList.add('error');
-        const divError = document.querySelector('#error-firstname');
-        divError.textContent='Debes completar el campo nombre';
+    if(name.value === ''){
+        // name.classList.add('error');
+        const divError = document.querySelector('#error-nombre');
+        divError.textContent='Falta agregar nombre y apellido...';
         validation = false;
     }
-    if(lastname.value === ''){
-        lastname.classList.add('error');
-        const divError = document.querySelector('#error-lastname');
-        divError.textContent='Debes completar el campo apellido';
-        validation= false;
+    if(email.value === ''){
+        // email.classList.add('error');
+        const divError = document.querySelector('#error-email');
+        divError.textContent='Falta agregar email...';
+        validation = false;
     }
-
+    if(age.value === 0){
+        // age.classList.add('error');
+        const divError = document.querySelector('#error-edad');
+        divError.textContent='Falta agregar edad...';
+        validation = false;
+    }
+   
+    
     if(validation){
-        formRegister.submit();
+        formNewsletter.submit();
     }
 }
 
-formRegister.addEventListener('submit',validationForm);
+formNewsletter.addEventListener('submit',validationForm);
